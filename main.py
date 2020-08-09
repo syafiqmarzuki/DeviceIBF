0ff9e92698ea85ebc29b0fae7d83545b2d4d262e
syafiqmarzuki <syafiqmarzuki33@gmail.com>
2020-08-10 06:57:28 +0700

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import timeit
 from datetime import datetime
 
 start = timeit.default_timer()
-srv.setup()
+#srv.setup()
 #cfgfire.setup()
 config = {
         "apiKey" : "AIzaSyBvtK_grzpMJFPd6HVhNVqLA9zf1xMYBGs",
@@ -22,30 +22,30 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-#twitter
-
+# #twitter
+# 
 auth = tweepy.OAuthHandler("8ZttJWOCRWYFc98DDBtK2K1xA", "f3z8UiSpqRklN2BkXubOd5yg6qZqZXC0zRugRALb8IWXEMpMrq")
 auth.set_access_token("1253167579650158593-6c7cZ1o0bfIILvblMwy8rpR38O0UFK", "0fHrd7ILuK5iJWIMApr0NkFi4Rzkn6w7vpnmi27xI4YBz")
 
 saat_ini = datetime.now()
 tgl = saat_ini.strftime('%d/%m/%Y') # format dd/mm/YY
-jam = saat_ini.strftime('%H:%M:%S')
+jam = saat_ini.strftime('%H:%M')
 url = "https://api.wassenger.com/v1/messages"
 url_get_phone = "https://ibflood.herokuapp.com/api/nohp"
 response = requests.get(url = url_get_phone)
 body = json.loads(str(response.text))
 phone_numbers = body['data']
-
-#fcm
-headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'key=AAAAa_ji2rI:APA91bFwwWcFsZaupSeZyZghKbN32x3Vp9QlKtHOpHfjY0Bo9Q1XeQTp_oN_c9NnUzsf3F3OpJ7oXsqsKQNbVBm4HXnwKwElhHPk2lBQ4h-Vjy_TljfCo1NKBZxukLGDeXPvHNt3JCks'
+# 
+# #fcm
+headerf = {
+        'Content-Type': "application/json",
+        'Authorization': "key=AAAAuHfdmTs:APA91bFCuAOeeCoOMfefqWLOkYvA62zakpdpcaEw-hgVHHufhL8RMGdf3-O3vBez-TAt5Wm9MTavJJoo-nWE17javt0zsaa72HD2HYUO4g7-PXPXud4xSgY_BEss4Mg624xLbP9J96de"
       }
 headerwa = {
     'content-type': "application/json",
     'token': "e634a1b525f85a007a6c49de61ccce530407c81c1f0b0f461789643ad00d33cede84ced7219c9354"
 }
-
+# 
 def convert_zero_to_plus_62(phone_number):
     if phone_number[0] == '0':
         string = phone_number[:0] + "+62" + phone_number[0+1:]
@@ -53,8 +53,8 @@ def convert_zero_to_plus_62(phone_number):
         return new_phone_number
     return phone_number
 
-
-
+# 
+# 
 
 # Create a tweet
     
@@ -66,17 +66,20 @@ while True:
        
         ults1 = 38 - ultra1
         ults2 = 38 - ultra2
-        print(ults1);
-        print(ults2);
+#         print('detib', ults1);
+#         print('sungai', ults2);
         #print('debit tumpah' , ults1);
         #print('sungai' ,  ults2);
         
         fults1 = '{:.3}'.format(ults1)
         fults2 = '{:.3}'.format(ults2)
         
+#         fultss1 = '{:.2}'.format(ults1)
+#         fultss2 = '{:.2}'.format(ults2)
+        
         stul1 =  status.fstatus(ults1,16,30)
         stul2 =  status.fstatus(ults2,13,25)
-        
+#         
         
         data = {
             "Raspi3/Sungai/":{
@@ -92,10 +95,10 @@ while True:
        # Create API object
         api = tweepy.API(auth)
         
-        if (ults1 > 34 and ults2 > 28):
-            
+        if (ults1 > 35 and ults2 > 26):
             pesan = "Pada tanggal "+str(tgl)+" , jam "+str(jam)+" . Ketinggian sekarang pada Debit Tumpah sudah mencapai "+str(fults1)+" cm, dan sungai sudah mencapai " +str(fults2) +" cm. Bahaya akan ada banjir datang, Harap untuk para warga menyelamatkan diri. #simulasibanjir #tugasakhir."
             api.update_status(pesan)
+            
             notif_isi = "Ketinggian sekarang pada Debit Tumpah sudah mencapai "+str(fults1)+" cm, dan sungai sudah mencapai " +str(fults2) +" cm. Bahaya akan ada banjir datang, Harap untuk para warga menyelamatkan diri. #simulasibanjir #tugasakhir."
             
             body = {
@@ -109,7 +112,7 @@ while True:
                 'data': {
                     'debit': fults1},
                     }
-            response = requests.post("https://fcm.googleapis.com/fcm/send",headers = headers, data=json.dumps(body))
+            response = requests.post("https://fcm.googleapis.com/fcm/send",headers = headerf, data=json.dumps(body))
             print(response.status_code)
             print(response.json())
             notif = {'isi_notif' : notif_isi}
@@ -122,9 +125,11 @@ while True:
                 res = requests.post(url = url, data=payload, headers=headerwa)
 #             #print('post twitter debit')
             
-        if (ults2 > 26):
+        elif (ults2 > 26):
             pesan = "Pada tanggal "+str(tgl)+" , jam "+str(jam)+" . Ketinggian sekarang pada Sungai sudah mencapai "+str(fults2)+" cm . Waspada air sungai akan meluber. Harap bersiap yang rumah nya pinggiran sungai untuk menyelamatkan diri."
             api.update_status(pesan)
+            
+            
             notif_isi = "Ketinggian sekarang pada Sungai sudah mencapai "+str(fults2)+" cm . Waspada air sungai akan meluber. Harap bersiap yang rumah nya pinggiran sungai untuk menyelamatkan diri."
             
             body = {
@@ -138,7 +143,7 @@ while True:
                 'data': {
                     'sungai': fults2 },
                     }
-            response = requests.post("https://fcm.googleapis.com/fcm/send",headers = headers, data=json.dumps(body))
+            response = requests.post("https://fcm.googleapis.com/fcm/send",headers = headerf, data=json.dumps(body))
             print(response.status_code)
             print(response.json())
             notif = {'isi_notif' : notif_isi}
@@ -161,19 +166,19 @@ while True:
             inreport = requests.post('https://ibflood.herokuapp.com/api/report/create', json=report)
             print("berhasil post db")
 #         
-#         
+        
         
         
         #mengatur otomatis pintu servo
-        if (ults1 > 25):
-            #print ("Banjir Kiriman Buka")
-            srv.ServoUp()
-        elif (ults2 <= 21 ):
-            #print ("Pintu Buka Terus")
-            srv.ServoUp()
-        elif (ults2 >= 20):
-            #print ("Tutup Pintu")
-            srv.ServoDown()
+#         if (ults1 > 25):
+#             #print ("Banjir Kiriman Buka")
+#             srv.ServoUp()
+#         elif (ults2 <= 21 ):
+#             #print ("Pintu Buka Terus")
+#             srv.ServoUp()
+#         elif (ults2 >= 20):
+#             #print ("Tutup Pintu")
+#             srv.ServoDown()
 #             
         
 
